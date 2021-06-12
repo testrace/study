00a4f4d2fafa196e33f5165eb54ffc83391df904
YONGJU <odetsuo@gmail.com>
2021-06-12 14:13:13 +0900

    [TDD] Add ExpiryDateCalculator 정해진 값을 리턴
    프로덕션 코드 구현 순서 첫번째
    calculateExpiryDate() 의 리턴을 하드코딩

---
+++ tdd/src/main/java/com/hadasht/study/tdd/subscription/ExpiryDateCalculator.java
@@ -0,0 +1,11 @@
+package com.hadasht.study.tdd.subscription;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+		return LocalDate.of(2020, 2, 1);
+	}
+
+}
---
+++ tdd/src/test/java/com/hadasht/study/tdd/subscription/ExpiryDateCalculatorTest.java
@@ -0,0 +1,34 @@
+package com.hadasht.study.tdd.subscription;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 요구사항
+ * 구독시스템
+ * 1. 구독료는 매달 1만원 선불이며 납부일 기준 한 달 뒤 서비스 만료일이 된다.
+ * 2. 2개월 이상 납부할 수 있다.
+ * 3. 10만 원을 납부하면 서비스를 1년 제공한다.
+ */
+class ExpiryDateCalculatorTest {
+
+	@Test
+	@DisplayName("만원 납부")
+	void pay10000won() throws Exception {
+		//given
+		LocalDate billingDate = LocalDate.of(2021, 1, 1);
+		int payAmount = 10_000;
+
+		//when
+		ExpiryDateCalculator calculator = new ExpiryDateCalculator();
+		LocalDate expiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
+
+		//then
+		assertEquals(LocalDate.of(2020,2,1), expiryDate);
+
+	}
+}
