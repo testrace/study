fcdbe484b43f86ecf4b2f35c8084f8b6dc8b4ee1
YONGJU <odetsuo@gmail.com>
2021-06-12 14:42:17 +0900

    [TDD] Add subscription PayData

---
+++ tdd/src/main/java/com/hadasht/study/tdd/subscription/PayData.java
@@ -0,0 +1,16 @@
+package com.hadasht.study.tdd.subscription;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+public class PayData {
+
+	private LocalDate billingDate;
+	private int payAmount;
+
+}
