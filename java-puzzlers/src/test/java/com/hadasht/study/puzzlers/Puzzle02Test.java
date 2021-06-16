7d8e25382d6db892b5243e30cf82a6cd2f2deefd
YONGJU <odetsuo@gmail.com>
2021-06-17 07:36:10 +0900

    [PUZZLE] Add puzzle02 변화를 위한 시간

--- java-puzzlers/src/test/java/com/hadasht/study/puzzlers/Puzzle02Test.java
+++ java-puzzlers/src/test/java/com/hadasht/study/puzzlers/Puzzle02Test.java
@@ -1,63 +1,64 @@
 package com.hadasht.study.puzzlers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import static com.hadasht.study.puzzlers.utils.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("변화를 위한 시간")
 class Puzzle02Test {
 
 	@SuppressWarnings("unused")
-	public static Stream<Arguments> isOdd() {
+	public static Stream<Arguments> change() {
 		return Stream.of(
-				Arguments.of(-2, false),
-				Arguments.of(-1, true),
-				Arguments.of(0, false),
-				Arguments.of(1, true),
-				Arguments.of(2, false)
+				Arguments.of(2.00, 1.10, 0.90),
+				Arguments.of(2.31, 1.41, 0.90)
 		);
 	}
 
 	@ParameterizedTest(name = PUZZLE_INDEX_ARGUMENTS_WITH_NAMES)
-	@MethodSource("isOdd")
-	void isOdd(int i, boolean isTrue) throws Exception {
-		boolean isOdd = Puzzle01.isOdd(i);
-		assertThat(isOdd).isEqualTo(isTrue);
+	@MethodSource("change")
+	void change(double x, double y, double answer) throws Exception {
+		double change = Puzzle02.change(x, y);
+		assertThat(change).isEqualTo(answer);
 	}
 
 	@ParameterizedTest(name = ANSWER_INDEX_ARGUMENTS_WITH_NAMES)
-	@MethodSource("isOdd")
-	void isOdd_Answer(int i, boolean isTrue) throws Exception {
-		boolean isOdd = Puzzle01.isOdd_Answer(i);
-		assertThat(isOdd).isEqualTo(isTrue);
+	@MethodSource("change")
+	void isOdd_Answer(double x, double y, double answer) throws Exception {
+		double v = Puzzle02.change_Answer(x, y);
+		assertThat(v).isEqualTo(answer);
 	}
 
 	@ParameterizedTest(name = SOLUTION_INDEX_ARGUMENTS_WITH_NAMES)
-	@MethodSource("isOdd")
-	void isOdd_Solution(int i, boolean isTrue) throws Exception {
-		boolean isOdd = Puzzle01.isOdd_Solution(i);
-		assertThat(isOdd).isEqualTo(isTrue);
+	@MethodSource("change")
+	void isOdd_Solution(double x, double y, double answer) throws Exception {
+		BigDecimal bigDecimal = Puzzle02.change_Solution(x, y);
+		assertThat(bigDecimal).isEqualTo(new BigDecimal(String.valueOf(answer)));
 	}
 
 	@SuppressWarnings("unused")
-	public static class Puzzle01 {
+	public static class Puzzle02 {
 
-		public static boolean isOdd(int i) {
-			return i % 2 == 1;
+		public static double change(double x, double y) {
+			return x - y;
 		}
 
-		public static boolean isOdd_Answer(int i) {
-			/* 나머지(%) 연산자는 좌항의 부호를 따라감 */
-			return Math.abs(i) % 2 == 1;
+		public static double change_Answer(double x, double y) {
+			/* 모르겠음 */
+			return 0.9;
 		}
 
-		public static boolean isOdd_Solution(int i) {
-			/* 비트 AND 연산자 */
-			return (i & 1) != 0;
+		public static BigDecimal change_Solution(double x, double y) {
+			BigDecimal bigDecimalX = new BigDecimal(String.valueOf(x));
+			BigDecimal bigDecimalY = new BigDecimal(String.valueOf(y));
+			return bigDecimalX.subtract(bigDecimalY);
 		}
 
 	}
