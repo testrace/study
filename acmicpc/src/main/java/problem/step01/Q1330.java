04c6fc7b0ff83e1085ab7452e4796cdff25a91df
YONGJU <odetsuo@gmail.com>
2021-12-04 23:39:30 +0900

    acmicpc problem : 1330

---
+++ acmicpc/src/main/java/problem/step01/Q1330.java
@@ -0,0 +1,28 @@
+package problem.step01;
+
+import java.util.Scanner;
+
+public class Q1330 {
+
+    public static String solution(int first, int second) {
+        if (first == second) {
+            return "==";
+        }
+        return greaterOrLess(first, second);
+    }
+
+    private static String greaterOrLess(int first, int second) {
+        if (first > second) {
+            return ">";
+        }
+        return "<";
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int first = scanner.nextInt();
+            int second = scanner.nextInt();
+            System.out.print(solution(first, second));
+        }
+    }
+}
---
+++ acmicpc/src/test/java/problem/step01/Q1330Test.java
@@ -0,0 +1,26 @@
+package problem.step01;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Q1330Test {
+
+    @ParameterizedTest(name = "{index} [{arguments}]")
+    @MethodSource
+    void test(int first, int second, String expected) throws Exception {
+        assertThat(Q1330.solution(first, second)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> test() {
+        return Stream.of(
+                Arguments.of(1, 2, "<"),
+                Arguments.of(10, 2, ">"),
+                Arguments.of(5, 5, "==")
+        );
+    }
+}
