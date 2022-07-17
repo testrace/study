7c57698f022f6406cb0820c2a18762eb56500680
YONGJU <odetsuo@gmail.com>
2022-07-17 14:20:23 +0900

    refactor: 테스트 코드 중복 제거
    프로더션 코드와 마찬가지로 테스트 코드에서도 중복을 제거해야 한다.

--- book-jwp-nextstep/src/test/java/nextstep/chapter02/CalculatorTest.java
+++ book-jwp-nextstep/src/test/java/nextstep/chapter02/CalculatorTest.java
@@ -2,20 +2,34 @@ package nextstep.chapter02;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        System.out.println("setUp");
+    }
+
     @Test
     void add() {
-        Calculator calculator = new Calculator();
         assertEquals(9, calculator.add(6, 3));
+        System.out.println("add");
     }
 
     @Test
     void subtract() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.subtract(6, 3));
+        System.out.println("subtract");
     }
 
+    @AfterEach
+    void teardown() {
+        System.out.println("teardown");
+    }
 }
