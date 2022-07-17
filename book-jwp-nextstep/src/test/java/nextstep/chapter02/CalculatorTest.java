6e8d4e24018af0be54faaccbe14a84be7256e22c
YONGJU <odetsuo@gmail.com>
2022-07-17 14:14:41 +0900

    test: 테스트 검증 자동화
    System.out.println 메서드는 실행 결과를 매번 눈으로 확인해야 한다.
    JUnit은 assertEquals()를 제공하여 결과를 자동으로 검증할 수 있다.

--- book-jwp-nextstep/src/test/java/nextstep/chapter02/CalculatorTest.java
+++ book-jwp-nextstep/src/test/java/nextstep/chapter02/CalculatorTest.java
@@ -1,5 +1,7 @@
 package nextstep.chapter02;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -7,13 +9,13 @@ class CalculatorTest {
     @Test
     void add() {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(6, 3));
+        assertEquals(9, calculator.add(6, 3));
     }
 
     @Test
     void subtract() {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.subtract(6, 3));
+        assertEquals(3, calculator.subtract(6, 3));
     }
 
 }
