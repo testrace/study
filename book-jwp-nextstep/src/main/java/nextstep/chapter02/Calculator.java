f5d119971638b7b843bc91b7ee9faa15f0a4c9cf
YONGJU <odetsuo@gmail.com>
2022-07-17 13:48:26 +0900

    refactor: 프로덕션 코드와 테스트 코드 분리
    Calculator 클래스의 main 메소드는 테스트를 위한 코드이다.
    프로덕션 코드와 테스트 코드는 배포 시점이 같을 필요가 없기에 분리한다.

--- book-jwp-nextstep/src/main/java/nextstep/chapter02/Calculator.java
+++ book-jwp-nextstep/src/main/java/nextstep/chapter02/Calculator.java
@@ -18,13 +18,5 @@ public class Calculator {
         return i / j;
     }
 
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(3, 4));
-        System.out.println(calculator.subtract(5, 4));
-        System.out.println(calculator.multiply(2, 6));
-        System.out.println(calculator.divide(8, 4));
-    }
-
 
 }
---
+++ book-jwp-nextstep/src/main/java/nextstep/chapter02/CalculatorTest.java
@@ -0,0 +1,13 @@
+package nextstep.chapter02;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(9, 3));
+        System.out.println(calculator.subtract(9, 3));
+        System.out.println(calculator.multiply(9, 3));
+        System.out.println(calculator.divide(9, 3));
+    }
+
+}
