872cdaf36cb09b7b9fc631ea615266e313d01e21
YONGJU <odetsuo@gmail.com>
2022-07-17 13:43:10 +0900

    feat: 간단한 사칙연산 계산기 구현

---
+++ book-jwp-nextstep/src/main/java/nextstep/chapter02/Calculator.java
@@ -0,0 +1,30 @@
+package nextstep.chapter02;
+
+public class Calculator {
+
+    int add(int i, int j) {
+        return i + j;
+    }
+
+    int subtract(int i, int j) {
+        return i - j;
+    }
+
+    int multiply(int i, int j) {
+        return i * j;
+    }
+
+    int divide(int i, int j) {
+        return i / j;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(3, 4));
+        System.out.println(calculator.subtract(5, 4));
+        System.out.println(calculator.multiply(2, 6));
+        System.out.println(calculator.divide(8, 4));
+    }
+
+
+}
