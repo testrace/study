4b1e01beaf261e5b745ea61272743a854513878b
YONGJU <odetsuo@gmail.com>
2022-07-22 12:13:12 +0900

    feat: 1단계 index.html

--- book-jwp-web-application-server/src/main/java/webserver/RequestHandler.java
+++ book-jwp-web-application-server/src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +32,15 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            final String requestLine = bufferedReader.readLine();
+            final String[] requestValues = requestLine.split(" ");
+
+            final Path path = Paths.get("book-jwp-web-application-server/webapp/" + requestValues[1]);
+            byte[] body = Files.readAllBytes(path);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
--- book-jwp-web-application-server/src/main/java/webserver/WebServer.java
+++ book-jwp-web-application-server/src/main/java/webserver/WebServer.java
@@ -10,13 +10,8 @@ public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+    public static void main(String[] args) throws Exception {
+        int port = setPortOrDefault(args);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
 
@@ -30,5 +25,15 @@ public class WebServer {
                 requestHandler.start();
             }
         }
+    }
+
+    private static int setPortOrDefault(final String[] args) {
+        int port;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+        return port;
     }
 }
